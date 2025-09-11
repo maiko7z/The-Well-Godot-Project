ab7408b51895675c4e19385e2a1e87b6a54bb0bb
maiko7z <lobs11489@gmail.com>
2025-09-10 21:04:54 -0400

    Sprint 1 Final Commit
    Final commit for Sprint 1 with working Main menu, start button, version notes, credits, and WPI Splash screen at the start.

--- firsttest/screens/main_menu.gd
+++ firsttest/screens/main_menu.gd
@@ -4,11 +4,10 @@ func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://world.tscn")
 
 func _on_version_notes_pressed() -> void:
-	pass # Replace with function body.
-
-
+	get_tree().change_scene_to_file("res://screens/version_notes.tscn")
+	
 func _on_credits_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://screens/credits.tscn")
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
