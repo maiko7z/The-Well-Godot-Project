2d05dcf2ac8d71bbe3d2e5fa0fb74e925dbaf5a1
cattcodes <bsanatewood@gmail.com>
2025-09-26 19:26:59 -0400

    First scene dialogue done!
    Just did some intro dialogue, still need to fix some bugs...

--- firsttest/scenes2d/levels2d/level_1.gd
+++ firsttest/scenes2d/levels2d/level_1.gd
@@ -2,17 +2,19 @@ extends Node2D
 #@onready var viewport = get_tree().get_root().get_viewport()
 @onready var dialogue_trigger_1: Area2D = $DialogueTrigger1
 
-#func _ready():
+func _ready():
+	Dialogic.start("scene1_timeline")
 	#DisplayServer.window_set_size(Vector2i(1152, 648))
 	#viewport.size = Vector2(1152,648)
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	#DisplayServer.window_set_size(Vector2i(480, 320))
 	#viewport.size = Vector2(480,320)
+	Dialogic.start("scene3_timeline")
 	get_tree().change_scene_to_file("res://scenes2d/levels2d/level_1_transition.tscn")
 	
 
 
 func _on_dialogue_trigger_1_body_entered(body: Node2D) -> void:
-	Dialogic.start("scene1_timeline")
+	Dialogic.start("scene2_timeline")
 	dialogue_trigger_1.queue_free() 
