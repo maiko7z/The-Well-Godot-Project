a7850b4bb0ab7e4467c8745ec5c66409d093dfc8
maiko7z <lobs11489@gmail.com>
2025-10-03 10:38:17 -0400

    Pause menu
    Pause menu added! needs to be added to other scenes

---
+++ firsttest/screens/pause_menu.gd
@@ -0,0 +1,38 @@
+extends Control
+
+func _ready():
+	$AnimationPlayer.play("RESET")
+	hide()
+	
+func resume():
+	get_tree().paused = false
+	$AnimationPlayer.play_backwards("blur")
+	hide()
+
+func pause():
+	get_tree().paused = true
+	$AnimationPlayer.play("blur")
+	show()
+
+func testEsc():
+	if Input.is_action_just_pressed("Pause") and get_tree().paused == false:
+		pause()
+		print("paused!")
+	elif Input.is_action_just_pressed("Pause") and get_tree().paused == true:
+		resume()
+		print("unpaused!")
+		
+
+func _on_resume_pressed() -> void:
+	resume()
+
+
+func _on_main_menu_pressed() -> void:
+	get_tree().change_scene_to_file("res://screens/main_menu.tscn")
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+	
+func _process(delta):
+	testEsc()
