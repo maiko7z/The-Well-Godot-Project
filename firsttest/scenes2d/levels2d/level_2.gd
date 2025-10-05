ce2f370351e2248cc42704842a586e7a0e82dd57
cattcodes <bsanatewood@gmail.com>
2025-10-05 15:57:35 -0400

    Dialogue updates and pause tinkering
    Fixing pause menu bugs

--- firsttest/scenes2d/levels2d/level_2.gd
+++ firsttest/scenes2d/levels2d/level_2.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
+	Dialogic.start("scene4extra_timeline")
 	get_tree().change_scene_to_file("res://scenes2d/levels2d/level_2_transition.tscn")
 	
 func _ready():
